2a8d0a3a388e4b273baa771b7daaa60dbe3475af
yunaichun <yunaichun@qingting.fm>
2022-09-14 10:39:49 +0800

    feat(NoTicket): remove babel

--- webpack-simple/babel-study/core.js
+++
@@ -1,9 +0,0 @@
-const parser = require('@babel/parser');
-const { transformFromAst } = require('@babel/core');
-
-const source = 'class Example {}';
-const AST = parser.parse(source);
-const { code } = transformFromAst(AST, null, {
-    presets: ['@babel/preset-env']
-});
-console.log(code);
