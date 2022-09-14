ec4b3ce5852721b194f841a1649b9d10658c7eaf
yunaichun <yunaichun@qingting.fm>
2022-09-14 11:27:02 +0800

    feat(NoTicket): update

--- webpack-simple/lib/test.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { getAST, getDependencis, transform } = require('./parser');
-
-const ast = getAST(path.join(__dirname, '../src/index.js'));
-const dependencis = getDependencis(ast);
-const source = transform(ast);
-console.log(ast);
-console.log(dependencis);
-console.log(source);
